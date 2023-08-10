6dce765c37f5e2d30eb3c03c991ba1b7abb7ec24
JustinFourie1993 <>
2023-08-10 08:19:31 +0000

    Delete code and start again

--- run.py
+++ run.py
@@ -1,11 +1,2 @@
-board = []
-
-for i in range(5):
-    board.append(["0"]*5)
-
-def make_grid(board):
-    for i in board:
-        print(" ".join(i))
-
-make_grid(board)
+from random import randint
 

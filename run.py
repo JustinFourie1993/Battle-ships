d690e83101d796fc9fc9834d1c33fedda0fa3876
JustinFourie1993 <>
2023-08-10 08:27:49 +0000

    create main board class

--- run.py
+++ run.py
@@ -1,2 +1,19 @@
 from random import randint
 
+
+class Board:
+    """
+    
+    """
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["0" for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self. guesses = []
+        self.ships = []
+
+    
+
+    
